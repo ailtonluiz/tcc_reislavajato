fb311912460642ba18b6e2500bce555b21f2fd4c
ailtonluiz <ailtonluiz@182d45b4-e3ef-4f24-b83d-b0951f157802>
2017-09-07 17:31:53 +0000

    Implentando a movimentação

--- reislavajato/src/main/java/br/com/reislavajato/controle/MovimentacaoControle.java
+++ reislavajato/src/main/java/br/com/reislavajato/controle/MovimentacaoControle.java
@@ -1,9 +1,9 @@
-/**
- * 
- */
 package br.com.reislavajato.controle;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,17 +11,18 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
+import org.omnifaces.util.Messages;
+
 import br.com.reislavajato.dao.CadastroDao;
 import br.com.reislavajato.dao.FuncionarioDao;
+import br.com.reislavajato.dao.MovimentoDao;
 import br.com.reislavajato.dao.ServicoDao;
-import br.com.reislavajato.dao.UsuarioDao;
 import br.com.reislavajato.dao.VeiculoDao;
 import br.com.reislavajato.entidade.Cadastro;
 import br.com.reislavajato.entidade.Funcionario;
 import br.com.reislavajato.entidade.ItemMovimento;
 import br.com.reislavajato.entidade.Movimento;
 import br.com.reislavajato.entidade.Servico;
-import br.com.reislavajato.entidade.Usuario;
 import br.com.reislavajato.entidade.Veiculo;
 
 /**
@@ -32,61 +33,121 @@ import br.com.reislavajato.entidade.Veiculo;
 @ManagedBean
 @ViewScoped
 public class MovimentacaoControle implements Serializable {
-	private UsuarioDao usuarioDao = new UsuarioDao();
-	private CadastroDao cadastroDao = new CadastroDao();
-	private VeiculoDao veiculoDao = new VeiculoDao();
-	private ServicoDao servicoDao = new ServicoDao();
-	private FuncionarioDao funcionarioDao = new FuncionarioDao();
 
-	private Funcionario funcionario;
+	ItemMovimento itemMovimento = new ItemMovimento();
+	ServicoDao servicoDao = new ServicoDao();
+	FuncionarioDao funcionarioDao = new FuncionarioDao();
+	VeiculoDao veiculoDao = new VeiculoDao();
+	CadastroDao cadastroDao = new CadastroDao();
+
 	private Movimento movimento;
-	private ItemMovimento itemMovimento;
-	private List<Usuario> usuarios;
-	private List<Cadastro> cadastros;
-	private List<Veiculo> veiculos;
+	private List<ItemMovimento> itensMovimento;
 	private List<Servico> servicos;
+	private List<Cadastro> cadastros;
 	private List<Funcionario> funcionarios;
+	private List<Veiculo> veiculos;
 
 	@PostConstruct
-	public void listar() {
-
-		veiculos = veiculoDao.listar("modelo");
-		cadastros = cadastroDao.listar("razaoSocial");
-		servicos = servicoDao.listar();
-		funcionarios = funcionarioDao.listar();
-
-	}
-
-	public void adicionar() {
-
-	}
-
-	public void remover() {
-
-	}
-
 	public void novo() {
+		try {
+			movimento = new Movimento();
+			movimento.setVlrTotal(new BigDecimal("0.00"));
+			servicos = servicoDao.listar("descricao");
+			itensMovimento = new ArrayList<>();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Não foi possível carregar as informações");
+			erro.printStackTrace();
+		}
+	}
 
+	public void adicionar(ActionEvent evento) {
+		Servico servico = (Servico) evento.getComponent().getAttributes().get("registroSelecionado");
+
+		int achou = -1;
+		for (int posicao = 0; posicao < itensMovimento.size(); posicao++) {
+			if (itensMovimento.get(posicao).getServico().equals(servico)) {
+				achou = posicao;
+			}
+		}
+		if (achou < 0) {
+			ItemMovimento itemMovimento = new ItemMovimento();
+			itemMovimento.setVlrParcial(servico.getVlrServico());
+			itemMovimento.setVlrComissao(servico.getPercComissao());
+			itemMovimento.setServico(servico);
+			itemMovimento.setQuantidade(new Short("1"));
+			itensMovimento.add(itemMovimento);
+		} else {
+			ItemMovimento itemMovimento = itensMovimento.get(achou);
+			itemMovimento.setQuantidade(new Short(itemMovimento.getQuantidade() + 1 + ""));
+			itemMovimento
+					.setVlrParcial(servico.getVlrServico().multiply(new BigDecimal(itemMovimento.getQuantidade())));
+			itemMovimento
+					.setVlrComissao(servico.getPercComissao().multiply(new BigDecimal(itemMovimento.getQuantidade())));
+		}
+
+		calcular();
+	}
+
+	public void remover(ActionEvent evento) {
+		ItemMovimento itemMovimento = (ItemMovimento) evento.getComponent().getAttributes().get("itemSelecionado");
+		int achou = -1;
+		for (int posicao = 0; posicao < itensMovimento.size(); posicao++) {
+			if (itensMovimento.get(posicao).getServico().equals(itemMovimento.getServico())) {
+				achou = posicao;
+			}
+		}
+		if (achou > -1) {
+			itensMovimento.remove(achou);
+		}
+		calcular();
+	}
+
+	public void calcular() {
+		movimento.setVlrTotal(new BigDecimal("0.00"));
+
+		for (int posicao = 0; posicao < itensMovimento.size(); posicao++) {
+			ItemMovimento itemMovimento = itensMovimento.get(posicao);
+			movimento.setVlrTotal(movimento.getVlrTotal().add(itemMovimento.getVlrParcial()));
+		}
+	}
+
+	public void listar() {
+		try {
+			funcionarios = funcionarioDao.listar();
+			veiculos = veiculoDao.listar();
+			cadastros = cadastroDao.listar();
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Não foi possível listar" + erro);
+			erro.printStackTrace();
+		}
+	}
+
+	public void finalizar() {
+		try {
+			movimento.setHorario(new Date());
+			listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Não foi possível finalizar a venda" + erro);
+			erro.printStackTrace();
+		}
 	}
 
 	public void salvar() {
+		try {
+			if (movimento.getVlrTotal().signum() == 0) {
+				Messages.addGlobalError("Valor total não pode ser 'R$: 0,00'");
+				return;
+			}
+			MovimentoDao movimentoDao = new MovimentoDao();
+			movimentoDao.salvar(movimento, itensMovimento);
+			novo();
+			Messages.addGlobalInfo("Operação realizada com sucesso!");
 
-	}
-
-	public void editar(ActionEvent evento) {
-
-	}
-
-	public void excluir(ActionEvent evento) {
-
-	}
-
-	public Movimento getMovimento() {
-		return movimento;
-	}
-
-	public void setMovimento(Movimento movimento) {
-		this.movimento = movimento;
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Não foi possível realizar está operação!" + erro);
+			erro.printStackTrace();
+		}
 	}
 
 	public ItemMovimento getItemMovimento() {
@@ -97,28 +158,20 @@ public class MovimentacaoControle implements Serializable {
 		this.itemMovimento = itemMovimento;
 	}
 
-	public List<Usuario> getUsuarios() {
-		return usuarios;
+	public Movimento getMovimento() {
+		return movimento;
 	}
 
-	public void setUsuarios(List<Usuario> usuarios) {
-		this.usuarios = usuarios;
+	public void setMovimento(Movimento movimento) {
+		this.movimento = movimento;
 	}
 
-	public List<Cadastro> getCadastros() {
-		return cadastros;
+	public List<ItemMovimento> getItensMovimento() {
+		return itensMovimento;
 	}
 
-	public void setCadastros(List<Cadastro> cadastros) {
-		this.cadastros = cadastros;
-	}
-
-	public List<Veiculo> getVeiculos() {
-		return veiculos;
-	}
-
-	public void setVeiculos(List<Veiculo> veiculos) {
-		this.veiculos = veiculos;
+	public void setItensMovimento(List<ItemMovimento> itensMovimento) {
+		this.itensMovimento = itensMovimento;
 	}
 
 	public List<Servico> getServicos() {
@@ -129,6 +182,14 @@ public class MovimentacaoControle implements Serializable {
 		this.servicos = servicos;
 	}
 
+	public List<Cadastro> getCadastros() {
+		return cadastros;
+	}
+
+	public void setCadastros(List<Cadastro> cadastros) {
+		this.cadastros = cadastros;
+	}
+
 	public List<Funcionario> getFuncionarios() {
 		return funcionarios;
 	}
@@ -137,11 +198,12 @@ public class MovimentacaoControle implements Serializable {
 		this.funcionarios = funcionarios;
 	}
 
-	public Funcionario getFuncionario() {
-		return funcionario;
+	public List<Veiculo> getVeiculos() {
+		return veiculos;
 	}
 
-	public void setFuncionario(Funcionario funcionario) {
-		this.funcionario = funcionario;
+	public void setVeiculos(List<Veiculo> veiculos) {
+		this.veiculos = veiculos;
 	}
+
 }
