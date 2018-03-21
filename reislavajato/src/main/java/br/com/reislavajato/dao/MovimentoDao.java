a743fead9baf4d500ef8d9ce95d13a3f503c5b16
ailtonluiz <ailtonluiz@182d45b4-e3ef-4f24-b83d-b0951f157802>
2018-03-21 00:00:45 +0000

    .

---
+++ reislavajato/src/main/java/br/com/reislavajato/dao/MovimentoDao.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package br.com.reislavajato.dao;
+
+/**
+ * @Criado por: ailtonluiz
+ * @Data: 20 de mar de 2018
+ */
+public interface MovimentoDao extends Persistencia(Movimento) {
+
+}
