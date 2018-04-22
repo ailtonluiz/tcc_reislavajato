e6336b67cc229fcc46533e9b2b263525f16b64d1
ailtonluiz <ailtonluiz@182d45b4-e3ef-4f24-b83d-b0951f157802>
2018-04-22 00:29:55 +0000

    Alterações 1. Adicionado mais cores no Enum. 2. Organizado os campos da tela de ordem de serviço. 3. Alterado o título da tela de lançamento de serviço.

--- trunk/reislavajato/src/main/java/br/com/reislavajato/enumeradores/EnumCor.java
+++ trunk/reislavajato/src/main/java/br/com/reislavajato/enumeradores/EnumCor.java
@@ -2,7 +2,8 @@ package br.com.reislavajato.enumeradores;
 
 public enum EnumCor implements Enumerador {
 
-	PRATA("01", "Prata"), PRETO("02", "Preto"), CINZA("03", "Cinza"), BRANCO("04", "Branco"), VERMELHO("05", "Vermelho"), AZUL("06", "Azul"), VERDE("07", "Verde");
+	PRATA("01", "Prata"), PRETO("02", "Preto"), CINZA("03", "Cinza"), BRANCO("04", "Branco"), VERMELHO("05",
+			"Vermelho"), AZUL("06", "Azul"), VERDE("07", "Verde"), AMARELO("08", "Amarelo"), OUTROS("09","Outros");
 
 	private final String abreviado;
 	private final String descricao;
