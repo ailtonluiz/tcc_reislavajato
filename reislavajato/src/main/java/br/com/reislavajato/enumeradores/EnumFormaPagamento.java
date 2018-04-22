8cc90b215726345c0a3309b33b01f55749af020d
ailtonluiz <ailtonluiz@182d45b4-e3ef-4f24-b83d-b0951f157802>
2018-04-22 01:06:45 +0000

    Alterações 1. Adicionado ao EnumFormaPagamento a opção Duplicata Mercantil. 2. Alterado na classe OrdemServico o enum padrão para a forma de pagamento. 3. Criado uma lista onde é possível listar as formas de pagamentos 4. Criado a tabView Dados de Pagamento

--- reislavajato/src/main/java/br/com/reislavajato/enumeradores/EnumFormaPagamento.java
+++ reislavajato/src/main/java/br/com/reislavajato/enumeradores/EnumFormaPagamento.java
@@ -5,7 +5,8 @@ public enum EnumFormaPagamento implements Enumerador {
 	DINHEIRO("01", "Dinheiro"), 
 	CARTAO_DEBITO("02", "Cartão de Débito"), 
 	CARTAO_CREDITO("03", "Cartão de Crédito"),
-	PIC_PAY("04", "Pic Pay");
+	PIC_PAY("04", "Pic Pay"),
+	DUPLICATA("05","Duplicata Mercantil");
 
 	private final String abreviado;
 	private final String descricao;
