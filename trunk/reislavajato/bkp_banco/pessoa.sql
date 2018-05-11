cb41105629860767fc80f8e3672bb35aed19e3c1
ailtonluiz <ailtonluiz@182d45b4-e3ef-4f24-b83d-b0951f157802>
2018-05-11 17:07:04 +0000

    Banco

---
+++ trunk/reislavajato/bkp_banco/pessoa.sql
@@ -0,0 +1,6 @@
+INSERT INTO pessoa VALUES
+(1, True, '2018-05-06', NULL, NULL, 'pedrohenriquevicenteosvaldodasneves_@abbott.com', NULL, NULL, 'Cliente', 'SIM', NULL, 'Pessoa Física', 'NAO', 1, 1, NULL, 1),
+(2, True, '2018-05-06', NULL, NULL, 'joanasandracastro@sercoti.com.br', NULL, NULL, 'Cliente', 'SIM', NULL, 'Pessoa Física', 'NAO', 2, 2, NULL, 2),
+(3, True, '2018-05-06', NULL, NULL, 'ricardooliverribeiro-90@focusgrafica.com.br', 'ricardo.ribeiro', NULL, 'Lavador', 'SIM', '123456', 'Pessoa Física', 'SIM', 3, 3, NULL, 3),
+(4, True, '2018-05-06', NULL, NULL, 'geraldoisaactomasdamata-97@rubens.adm.br', NULL, NULL, 'Lavador', 'SIM', NULL, 'Pessoa Física', 'NAO', 4, 4, NULL, 4),
+(5, True, '2018-05-06', NULL, NULL, 'bbentoryanmonteiro@i9pneus.com.br', NULL, NULL, 'Lavador', 'SIM', NULL, 'Pessoa Física', 'NAO', 5, 5, NULL, 5);
---
+++ trunk/reislavajato/bkp_banco/pessoa_fisica.sql
@@ -0,0 +1,6 @@
+INSERT INTO pessoa_fisica VALUES
+(1, '960.911.991-37', '2007-05-01', '1984-08-18', 'Segundo Grau', 'Casado(a)', 'Pedro Henrique Vicente Osvaldo das Neves', 'Eduarda Lívia ', 'Danilo Leandro Lorenzo das Neves', 'SSP', '35759507', 'Masculino', 'Goiás'),
+(2, '581.461.321-11', '2000-01-06', '1993-11-17', 'Segundo Grau', 'Casado(a)', 'Joana Sandra Castro', 'Nicole Sebastiana ', 'Anthony Erick Marcos Vinicius Castro', 'SSP', '46757965', 'Feminino', 'Goiás'),
+(3, '977.143.461-67', '2006-02-05', '1993-03-03', 'Segundo Grau', 'Solteiro(a)', 'Ricardo Oliver Ribeiro', 'Eloá Bianca Isabela ', 'Julio Pietro Oliver Ribeiro', 'SSP', '401412659', 'Masculino', 'Goiás'),
+(4, '704.767.601-51', '2009-06-19', '2000-06-19', 'Segundo Grau', 'Solteiro(a)', 'Geraldo Isaac Tomás da Mata', 'Lara Sebastiana Manuela ', 'Rafael Danilo da Mata', 'SSP', '270421679', 'Masculino', 'Goiás'),
+(5, '390.206.131-62', '2000-06-05', '1982-12-09', 'Graduação', 'Casado(a)', 'Bento Ryan Monteiro', 'Hadassa Isadora Fabiana ', 'Filipe Luan Monteiro', 'SSP', '403162506', 'Masculino', 'Goiás');
