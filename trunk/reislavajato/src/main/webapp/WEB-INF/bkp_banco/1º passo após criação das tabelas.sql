8db85b411b166ba253a4071c88577cbe62a118fe
ailtonluiz <ailtonluiz@182d45b4-e3ef-4f24-b83d-b0951f157802>
2018-05-21 00:07:11 +0000

    ATENÇÃO Ao atualizar o projeto drop o banco "lavajato", execute o serviço do tomcat, após a criação das tabelas, execute o script "1º passo após criação das tabelas"
    Após isso, faça o insert com os arquivos .sql

---
+++ trunk/reislavajato/src/main/webapp/WEB-INF/bkp_banco/1º passo após criação das tabelas.sql
@@ -0,0 +1,19 @@
+ALTER TABLE `lavajato`.`cargo_aud` 
+CHANGE COLUMN `cargo_aud_id` `cargo_aud_id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
+
+ALTER TABLE `lavajato`.`financeiro_aud` 
+CHANGE COLUMN `financeiro_aud_id` `financeiro_aud_id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
+
+
+ALTER TABLE `lavajato`.`ordem_servico_aud` 
+CHANGE COLUMN `ordem_servico_aud_id` `ordem_servico_aud_id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
+
+
+ALTER TABLE `lavajato`.`servico_aud` 
+CHANGE COLUMN `servico_aud_id` `servico_aud_id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
+
+
+ALTER TABLE `lavajato`.`cliente_aud` 
+CHANGE COLUMN `cliente_aud_id` `cliente_aud_id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
+
+
