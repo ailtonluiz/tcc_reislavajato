77d2604f06b8eb75cd6955a60adbd5e8c52b1e83
ailtonluiz <ailtonluiz@182d45b4-e3ef-4f24-b83d-b0951f157802>
2018-05-22 16:54:13 +0000

    Trigger

--- trunk/reislavajato/bkp_banco/trg_aud_ordem_servico_delete.sql
+++ trunk/reislavajato/bkp_banco/trg_aud_ordem_servico_delete.sql
@@ -3,13 +3,13 @@ CREATE TRIGGER trg_aud_ordem_servico_delete
 BEFORE DELETE ON ordem_servico
 FOR EACH ROW BEGIN
 INSERT INTO ordem_servico_aud
-SET processo_operacao = 'Exclus√£o',
+SET processo_operacao = 'Exclus„o',
 obs_os = OLD.obs_os,
 os_id = OLD.os_id,
 vlr_total = OLD.vlr_total,
 pct_desconto = OLD.pct_desconto,
 obs_os = OLD.obs_os,
 usuario_operacao = 'Administrador',
-tela = 'Tela Exclus√£o',
+tela = 'Tela Exclus„o',
 dt_operacao = NOW(); END$$
 DELIMITER ;
