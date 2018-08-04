2d6387bbdcb621cc193a081f7113efae649fdc85
ailtonluiz <ailtonluiz@182d45b4-e3ef-4f24-b83d-b0951f157802>
2018-08-04 18:36:22 +0000

    Fazendo alguns ajustes para aproveitar o projeto

---
+++ trunk/reislavajato/src/main/java/br/com/reislavajato/dao/ComissaoDao.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package br.com.reislavajato.dao;
+
+import br.com.reislavajato.entidade.Cargo;
+
+/**
+ * @Criado por: ailtonluiz
+ * @Data: 13 de ago de 2017
+ */
+public interface ComissaoDao extends Persistencia<Cargo> {
+
+}
